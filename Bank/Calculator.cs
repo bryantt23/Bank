45ff8425e0c0293e1ea6064ac8784e5b377ca1b8
bryantt23 <bryantt23@DESKTOP-7I0G27J>
2018-04-09 02:00:42 -0700

    Add project files.

---
+++ Bank/Calculator.cs
@@ -0,0 +1,11 @@
+﻿namespace Calculator
+{
+    public class Calculator
+    {
+
+        public static int Add(string numbers)
+        {
+            return 0;
+        }
+    }
+}
---
+++ Test/UnitTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Test
+{
+    [TestClass]
+    public class UnitTests
+    {
+        [TestMethod]
+        public void Empty_String_Returns_Zero()
+        {
+            //Arrange
+            int expected = 0;
+
+            //Act
+            int actual = Calculator.Calculator.Add("");
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+    }
+}
